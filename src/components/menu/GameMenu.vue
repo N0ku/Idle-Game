932575d68d4642484d0840de7aab8c21ffed6975
Elim Florvil <elim.florvil@icloud.com>
2023-10-03 21:25:47 +0200

    home page

---
+++ src/components/menu/GameMenu.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container-button">
+    <button>INSCRIPTION</button>
+    <button>CONNEXION</button>
+  </div>
+</template>
+<style scoped>
+.container-button {
+  position: absolute;
+  top: 45%;
+  left: 40%;
+}
+button {
+  margin: 10px 0px;
+  background-color: #795548;
+  padding: 10px 40px;
+  border-radius: 5px;
+  transition: all 500ms ease-in-out;
+  color: grey;
+}
+button:hover {
+  background-color: #4caf50a8;
+  color: black;
+}
+</style>
