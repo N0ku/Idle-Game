2557e0d4b3d06b6ff93a6f2a690861d6f84beef0
Eddy Muzaffar <eddy.muzaffar@outlook.com>
2023-10-04 12:14:53 +0200

    init taildwind

--- client/src/views/GameView.vue
+++ client/src/views/GameView.vue
@@ -1,3 +1,7 @@
 <template>
     <h1>game</h1>
+  <DrawerGlobalView></DrawerGlobalView>
 </template>
+<script setup lang="ts">
+import DrawerGlobalView from "@/components/game/drawer/DrawerGlobalView.vue";
+</script>
