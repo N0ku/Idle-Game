1c3fdbdb49dabe2b2556c7bf4770d958cbd29928
Elim Florvil <elim.florvil@icloud.com>
2023-10-04 10:30:45 +0200

    update path Game

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import LoginView from '../views/LoginView.vue'
+import GameView from '../views/GameView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/login',
       name: 'Login',
       component: LoginView
+    },
+    {
+      path: '/game',
+      name: 'Game',
+      component: GameView
     }
   ]
 })
