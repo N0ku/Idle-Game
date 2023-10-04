a4ee4730161e1eea65542adc29db9161eed36661
Eddy Muzaffar <eddy.muzaffar@outlook.com>
2023-10-04 11:40:44 +0200

    add config taildwin

--- src/components/game/drawer/DrawerGlobalView.vue
+++ src/components/game/drawer/DrawerGlobalView.vue
@@ -27,17 +27,20 @@ function toogle(){
             class="w-12 h-48 p-1 my-auto rounded text-white bg-gray-600 text-center focus:outline-none hover:bg-gray-500 transition-color duration-300"
         >
 					<span
-              :class="[right ? '-rotate-90' : 'rotate-90']"
+              :class="[open.value ? '-rotate-180' : '']"
               class="block transform origin-center font-bold"
           >
-						Drawer
+            <!-- Chevron Icone -->
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+               </svg>
+
 					</span>
         </button>
 
         <!-- Sidebar Content -->
         <div
-            ref="content"
-            class="transition-all max-w-lg duration-700 bg-yellow-200 overflow-hidden flex items-center justify-center"
+            class="transition-all max-w-xlg duration-700 bg-yellow-200 overflow-hidden flex"
             v-if="open.value"
         >
           <div class="w-48 text-center font-bold text-xl">Sidebar</div>
