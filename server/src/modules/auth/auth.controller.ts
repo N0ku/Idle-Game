e96ba3998f1cad7ccd79eff0feb71f94029fd907
Elim Florvil <elim.florvil@icloud.com>
2023-10-04 13:54:39 +0200

    add cookie

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -4,23 +4,39 @@ import { login, register } from "./auth.services";
 import { requireLogin } from "./auth.middleware";
 
 export function registerAuthRoutes(app: Express) {
-    
-    // on enregistre une route /auth/register
-    // .                                        TypeParams, TypeQuery, TypeBody
-    app.post('/auth/register', async (req: Request<unknown, unknown, AuthRegisterBody>, res: Response) => {
-        
-        // on call le service auth.register
-        const result = await register(req.body)
-        // on reponds a la requete http avec le result
-        res.json(result)
-    })
-    
-    app.post('/auth/login', async (req, res) => {
-        const result = await login(req.body)
-        res.json(result)
-    })
-    
-    app.get('/auth/me', requireLogin, (req, res) => {
-        res.json(req.user)
-    })
+  // on enregistre une route /auth/register
+  // .                                        TypeParams, TypeQuery, TypeBody
+  app.post(
+    "/auth/register",
+    async (req: Request<unknown, unknown, AuthRegisterBody>, res: Response) => {
+      // on call le service auth.register
+      const result = await register(req.body);
+
+      // on set un cookie si on a un token dans le result
+      if (result.token) {
+        res.cookie("token", result.token, {
+          expires: new Date(+new Date() + 1000000000),
+          sameSite: "none",
+        });
+      }
+      // on reponds a la requete http avec le result
+      res.json(result);
+    }
+  );
+
+  app.post("/auth/login", async (req, res) => {
+    const result = await login(req.body);
+    // on set un cookie si on a un token dans le result
+    if (result.token) {
+      res.cookie("token", result.token, {
+        expires: new Date(+new Date() + 1000000000),
+        sameSite: "none",
+      });
+    }
+    res.json(result);
+  });
+
+  app.get("/auth/me", requireLogin, (req, res) => {
+    res.json(req.user);
+  });
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,28 +1,36 @@
-import express from 'express'
-import cors from 'cors'
-import { registerAuthRoutes } from './modules/auth/auth.controller'
-import { isLogin } from './modules/auth/auth.middleware'
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { registerAuthRoutes } from "./modules/auth/auth.controller";
+import { isLogin } from "./modules/auth/auth.middleware";
 
 export function initWebServer() {
-    // Creation du serveur http
-    const app = express()
-    
-    // Utilise le plugin CORS
-    app.use(cors())
-    
-    // permet de décoder le contenu des requetes http (de type JSON)
-    app.use(express.json())
-    
-    // Add isLogin middleware
-    app.use(isLogin)
+  // Creation du serveur http
+  const app = express();
 
-    // On enregistre nos controllers
-    registerAuthRoutes(app)
-    
-    // On ecoute sur le port configuré avec le .env
-    app.listen(process.env.NODE_PORT, () => {
-        console.log(`Listening on http://localhost:${process.env.NODE_PORT}`)
+  // Utilise le plugin CORS
+  app.use(
+    cors({
+      credentials: true,
     })
-    
-    return app;
+  );
+
+  // lire les cookies
+  app.use(cookieParser());
+
+  // permet de décoder le contenu des requetes http (de type JSON)
+  app.use(express.json());
+
+  // Add isLogin middleware
+  app.use(isLogin);
+
+  // On enregistre nos controllers
+  registerAuthRoutes(app);
+
+  // On ecoute sur le port configuré avec le .env
+  app.listen(process.env.NODE_PORT, () => {
+    console.log(`Listening on http://localhost:${process.env.NODE_PORT}`);
+  });
+
+  return app;
 }
