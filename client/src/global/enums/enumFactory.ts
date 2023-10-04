2d7faba26b89be7df8973ef560b37d7e2eedc765
thomlam <lamiablethomas@gmail.com>
2023-10-04 15:17:15 +0200

    feature/Add local processus for factory

---
+++ client/src/global/enums/enumFactory.ts
@@ -0,0 +1,20 @@
+export enum Products {
+    Wood,
+    Stone,
+    Weed,
+    CBD,
+    Water,
+    Fertilizer,
+    // TODO: Add Ceramic pot
+}
+
+export enum TypeFactory {
+    WoodProduction,
+    StoneProduction,
+    WeedProduction,
+    CBDProduction,
+    WaterProduction,
+    FertilizerProduction,
+}
+
+
--- client/src/global/implements.ts
+++ client/src/global/implements.ts
@@ -2,11 +2,17 @@
 
 import {TestClasses} from "./classes/template"
 import type { TestInterface } from "./interface/template";
+import type {  FactoryInfo } from "./interface/interfaceFactoryInfo";
+
 import { enumTest } from "./enums/template";
+import { Products, TypeFactory } from "./enums/enumFactory";
+
 
 
 export { TestClasses }; // Ne pas oublier de export !! 
-export type { TestInterface }; // Ne pas oublier de export!! 
-export { enumTest }; // Ne pas oublier de export!! 
+export type { TestInterface, FactoryInfo }; // Ne pas oublier de export!! 
+export { enumTest, Products, TypeFactory }; // Ne pas oublier de export!! 
 
+
+// Pour import implements :  import * as Implements from '../global/implements';
 
