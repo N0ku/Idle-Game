b66fa87625a6d8116bd96d3bed44088ed1cbba8d
thomlam <lamiablethomas@gmail.com>
2023-10-04 11:03:15 +0200

    install / add global template and tsSetting

---
+++ src/global/classes/template.ts
@@ -0,0 +1,11 @@
+export class TestClasses {
+
+    //TODO ATTENTION, NE PAS OUBLIER EXPORT
+
+    test:string;
+    constructor(test:string){
+        this.test= test;
+    }
+
+    // Add méthodes de travail, get, set etc..
+}
