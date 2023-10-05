905fa8c8a0dc8051238e3ae975a94c4dbbf5f1be
N0ku <naku.pocket@gmail.com>
2023-10-05 15:11:14 +0200

    feat/add automatic get data of user, add three first factories

--- client/src/views/GameView.vue
+++ client/src/views/GameView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="game-container">
-    <StartSlider :start="start" @itemClicked="handleItemClicked" v-if="start" />
+    <StartSlider :start="start" :currentStep="counterStep" @itemClicked="handleItemClicked" v-if="start" />
     <DrawerGlobalView />
   </div>
 </template>
@@ -9,21 +9,56 @@
 import StartSlider from "../components/game/StartSlider.vue";
 import DrawerGlobalView from "@/components/game/drawer/DrawerGlobalView.vue";
 import { useUserStore } from "@/stores/datastore";
-import { ref } from "vue";
-import { Factory,Product,TypeFactory } from "../../../server/src/global/implements";
+import { reactive, ref } from "vue";
+import { Factory, TypeFactory, Products } from "../../../server/src/global/implements";
 
 const userStore = useUserStore();
 
 
 let start = ref(true);
+let counterStep = ref(1);
 
-let handleItemClicked = (product: Product): void => {
-  start.value = false;
-  let newFactory = new Factory(product.name, TypeFactory.StoneProduction, "0", 10, 1, undefined, 1);
+let handleItemClicked = (product: any): void => {
+  let factoryType: TypeFactory = TypeFactory.WoodProduction;
+  let newFactory: Factory = new Factory(product.name, factoryType, userStore.getId, 10, 1, undefined, counterStep.value);
+
+  switch (product.id) {
+    case Products.Wood:
+      factoryType = TypeFactory.WoodProduction;
+      newFactory = new Factory(product.title, factoryType, userStore.getId, 20, 1, undefined, counterStep.value);
+      break;
+    case Products.Stone:
+      factoryType = TypeFactory.StoneProduction;
+      newFactory = new Factory(product.title, factoryType, userStore.getId, 20, 1, undefined, counterStep.value);
+      break;
+    case Products.CBD:
+      factoryType = TypeFactory.CBDProduction;
+      newFactory = new Factory(product.title, factoryType, userStore.getId, 5, 1, undefined, counterStep.value);
+      break;
+    case Products.Fertilizer:
+      factoryType = TypeFactory.FertilizerProduction;
+      newFactory = new Factory(product.title, factoryType, userStore.getId, 15, 1, undefined, counterStep.value);
+      break;
+    case Products.Weed:
+      factoryType = TypeFactory.WeedProduction;
+      newFactory = new Factory(product.title, factoryType, userStore.getId, 5, 1, undefined, counterStep.value);
+      break;
+    case Products.Water:
+      factoryType = TypeFactory.WaterProduction;
+      newFactory = new Factory(product.title, factoryType, userStore.getId, 15, 1, undefined, counterStep.value);
+      break;
+    default:
+      break;
+  }
+
   userStore.addFactory({ factory: newFactory });
-
-
+  counterStep.value++;
+  if (counterStep.value > 3) {
+    start.value = false;
+  }
 };
+
+const factories = reactive<Factory[]>(userStore.getFactories);
 </script>
 
 <style lang="scss">
@@ -33,4 +68,3 @@ let handleItemClicked = (product: Product): void => {
   background-image: url('https://media.discordapp.net/attachments/1158387777868660736/1158931549635485837/pixel-art-grassland-powerpoint-background-.jpg?ex=651e0a9d&is=651cb91d&hm=f529110f9c9c4e73f7781c95b698148c4d55c12bfea06a5dbfe9d00c1774b42e&=&width=2300&height=1294');
 }
 </style>
-../../../global/implements
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
 import GameMenu from '@/components/menu/GameMenu.vue'
 import TitleMenu from '@/components/menu/TitleMenu.vue'
-import {isLogin} from '../Redirect'
-
-isLogin() 
 </script>
 
 <template>
