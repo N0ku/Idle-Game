9eb6257d8ba6ce3d0d5e10456daff31511e1cad1
Eddy Muzaffar <eddy.muzaffar@outlook.com>
2023-10-06 11:01:50 +0200

    push service pour echange

---
+++ server/src/modules/echange/echange.services.ts
@@ -0,0 +1,20 @@
+
+import { db } from "../../db/mongo";
+import {Echange} from "@/global/classes/Echange";
+
+export async function getAllEchange() {
+    const Echange = db!.collection<Echange>("echange");
+
+    const echange = await Echange.find();
+
+    return { echange };
+}
+
+export async function addEchange(body: Echange) {
+    const Echange = db!.collection<Echange>("echange");
+    await Echange.insertOne(body);
+    const result = await Echange.findOne(body);
+    return { result };
+}
+
+
