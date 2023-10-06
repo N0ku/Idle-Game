10f128f082bb2167030437399049c9b63a0fe44d
Elim Florvil <elim.florvil@icloud.com>
2023-10-06 11:13:42 +0200

    Update enumFactory.ts

--- server/src/global/enums/enumFactory.ts
+++ server/src/global/enums/enumFactory.ts
@@ -1,3 +1,5 @@
+import { Factory, Product, UserStage } from "../implements";
+
 export enum Products {
   Wood = "Wood",
   Stone = "Stone",
@@ -8,7 +10,7 @@ export enum Products {
 }
 
 export class ProductsExtensions {
-  public static GetImage(product: Products ) {
+  public static GetImage(product: Products) {
     switch (product) {
       case Products.Wood:
         return "src/assets/img/products/wood.png";
@@ -27,7 +29,7 @@ export class ProductsExtensions {
     }
   }
 
-  public static GetPrice(product: Products | undefined) {
+  public static GetBasePrice(product: Products | undefined) {
     switch (product) {
       case Products.Wood:
         return 1;
@@ -42,8 +44,17 @@ export class ProductsExtensions {
       case Products.Fertilizer:
         return 7.5;
       default:
-        return;
+        return 0;
     }
+  }
+  public static GetPrice(product: Products | undefined, quantity: number = 1) {
+    const basePrice = this.GetBasePrice(product);
+
+    if (basePrice !== undefined) {
+      return basePrice * quantity;
+    }
+
+    return 0;
   }
 }
 
@@ -68,9 +79,9 @@ export class TypeFactoryExtensions {
       case TypeFactory.CBDProduction:
         return "src/assets/img/factories/cbd_factories.png";
       case TypeFactory.WaterProduction:
-        return "water.png";
+        return "src/assets/img/factories/water_factories.png";
       case TypeFactory.FertilizerProduction:
-        return "fertilizer.png";
+        return "src/assets/img/factories/fertilizer_factories.png";
       default:
         return "";
     }
@@ -93,11 +104,10 @@ export class TypeFactoryExtensions {
         return;
     }
   }
-  public static GetPriceUpgrade(
-    factory: TypeFactory | undefined,
-    level: number
-  ) {
-    switch (factory) {
+  public static GetPriceUpgrade(factory: Factory) {
+    const rawLevel = factory.getLevel();
+    const level = typeof rawLevel === "number" ? rawLevel : 1;
+    switch (factory.getFactoryType()) {
       case TypeFactory.WoodProduction:
         return 100.0 * Math.pow(2, level - 1);
       case TypeFactory.StoneProduction:
@@ -111,7 +121,125 @@ export class TypeFactoryExtensions {
       case TypeFactory.FertilizerProduction:
         return 75.0 * Math.pow(2, level - 1);
       default:
-        return;
+        return 0;
     }
+  }
+  public static CanUpgradeFactory(
+    money: number,
+    factory: Factory,
+    user: UserStage
+  ): boolean {
+    const upgradePrice = TypeFactoryExtensions.GetPriceUpgrade(factory);
+    const rawLevel = factory.getLevel();
+    const level = typeof rawLevel === "number" ? rawLevel : 1;
+
+    if (level >= 5) {
+      const requiredResources =
+        TypeFactoryExtensions.getRequiredProductsForUpgrade(factory);
+
+      if (
+        money >= upgradePrice &&
+        TypeFactoryExtensions.hasRequiredResources(
+          requiredResources,
+          user.products
+        )
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      if (money >= upgradePrice) {
+        return true; // Vous pouvez effectuer l'upgrade
+      } else {
+        return false; // Vous ne pouvez pas effectuer l'upgrade
+      }
+    }
+  }
+
+  public static getRequiredProductsForUpgrade(factory: Factory): Product[] {
+    const rawLevel = factory.getLevel();
+    const level = typeof rawLevel === "number" ? rawLevel : 1;
+    const requiredProducts: Product[] = [];
+    const productRequirements: Record<
+      TypeFactory,
+      { first: Products; second: Products }
+    > = {
+      [TypeFactory.WoodProduction]: {
+        first: Products.Wood,
+        second: Products.Stone,
+      },
+      [TypeFactory.StoneProduction]: {
+        first: Products.Stone,
+        second: Products.Wood,
+      },
+      [TypeFactory.WeedProduction]: {
+        first: Products.Wood,
+        second: Products.Water,
+      },
+      [TypeFactory.CBDProduction]: {
+        first: Products.Wood,
+        second: Products.Stone,
+      },
+      [TypeFactory.FertilizerProduction]: {
+        first: Products.Stone,
+        second: Products.Wood,
+      },
+      [TypeFactory.WaterProduction]: {
+        first: Products.Wood,
+        second: Products.Water,
+      },
+    };
+
+    const type = factory.getFactoryType();
+    const requirements = productRequirements[type];
+
+    if (requirements) {
+      const firstQuantity = 50 * level;
+      const secondQuantity = 25 * level;
+
+      requiredProducts.push({
+        name: requirements.first,
+        price: 0,
+        description: "",
+        quantity: firstQuantity,
+      });
+
+      requiredProducts.push({
+        name: requirements.second,
+        price: 0,
+        description: "",
+        quantity: secondQuantity,
+      });
+    }
+
+    return requiredProducts;
+  }
+
+  public static hasRequiredResources(
+    requiredResources: Product[],
+    availableResources: Product[]
+  ): boolean {
+    // Créez un dictionnaire pour stocker les ressources disponibles pour une recherche plus rapide
+    const availableResourcesMap: Record<string, number> = {};
+
+    for (const resource of availableResources) {
+      availableResourcesMap[resource.name] = resource.quantity;
+    }
+
+    // Parcourez les ressources requises
+    for (const requiredResource of requiredResources) {
+      const { name, quantity } = requiredResource;
+
+      // Vérifiez si la ressource requise existe dans les ressources disponibles
+      if (
+        availableResourcesMap[name] === undefined ||
+        availableResourcesMap[name] < quantity
+      ) {
+        return false; // L'une des ressources requises n'est pas disponible en quantité suffisante
+      }
+    }
+
+    return true; // Toutes les ressources sont disponibles en quantité suffisante
   }
 }
